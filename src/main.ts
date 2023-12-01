6262e0b64a710feb93be0206acf56d419c447827
yuluo <568055454@qq.com>
2023-12-02 05:35:25 +0800

    feat: 跨域的两种实现方式

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { HttpFilter } from './common/filter';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+// import * as cors from 'cors';
 
 async function bootstrap() {
   // 添加NestExpressApplication泛型，扩展属性的类型推导。
   // 不然eslint会报错useStaticAssets不存在。
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 配置允许跨域
+  app.enableCors();
+
+  // 使用第三方库cors实现跨域
+  // app.use(cors());
 
   // 使用静态目录。src/public下的所有文件都可以直接访问
   app.useStaticAssets(join(__dirname, '..', 'public'));
